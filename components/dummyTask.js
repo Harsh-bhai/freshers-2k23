316684b919c005ef84f1b216bf3dee44ec1b8fe3
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-10-26 18:55:54 +0530

    date updated

--- components/dummyTask.js
+++ components/dummyTask.js
@@ -11,9 +11,12 @@ const DummyTask = () => {
             you best of luck for the tasks 👍🏻 hope you all enjoy🎉🥳
           </span>
           <span className="text-white  text-2xl text-center">
-          First task coming on 
+          Be ready on 
           </span>
-          <span className='font-bold text-2xl text-white '> 28th October🔥🔥..</span>
+          <span className='font-bold text-2xl text-white '> 30th October🔥🔥..</span>
+          <span className="text-white  text-2xl text-center">
+          A massive surprise is waiting for you 💪💪!! 
+          </span>
           <span className="text-white text-2xl text-center">
           All the best & Stay Tuned...!!
           </span>
